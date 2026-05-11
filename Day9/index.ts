ee1118dbd1c931c5387cec661782497fdb4422c6
Kunam-Satya-Varshini <varshukunam@gmail.com>
2026-05-11 12:43:31 +0530

    basics os ts added

---
+++ Day9/index.ts
@@ -0,0 +1,10 @@
+let name: string = "Varshini";
+let result: number | string;
+result = 45;
+result = "success";
+console.log(name, result);
+ 
+function add(a: number, b: number): string {
+    return "Sum: " + (a + b);
+}
+console.log(add(5, 10));
