b2c767a3fffa7bc43d85567c2a19f99fcd2c6ec9
Kunam-Satya-Varshini <varshukunam@gmail.com>
2026-05-18 12:22:07 +0530

    annotations in a test

---
+++ Day15/tests/practice.spec.js
@@ -0,0 +1,23 @@
+//  import { test, expect } from '@playwright/test';
+//  test('amazon razor products check', async ({ page }) => {
+//     await page.goto('https://www.amazon.com/');
+
+//    const txtBox = page.locator('#twotabsearchtextbox');
+//    await txtBox.fill("gaming");
+//    const subBtn = page.locator('#nav-search-submit-button');
+//       await subBtn.click();
+//    await page.waitForTimeout(5000);
+
+//  });
+
+// test('amazon test', async ({ page }) => {
+//   await page.goto('https://www.amazon.com/');
+//   await page.waitForTimeout(3000);
+
+//   await page.fill('#twotabsearchtextbox', 'gaming');
+//   await page.waitForTimeout(2000);
+
+//   await page.click('#nav-search-submit-button');
+//   await page.waitForTimeout(5000);
+// });
+
